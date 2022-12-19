2db16a315d951842c9cec2bc17f786bfcad8d132
DenisWilk <info.d1@tut.by>
2022-12-19 20:10:13 +0300

    feat: implement read file

---
+++ src/commands/cat.js
@@ -0,0 +1,27 @@
+import fs, { createReadStream } from "fs/promises";
+import path from "path";
+import { currentDir, errorMessage } from "../index.js";
+
+export const cat = async (command) => {
+  const enteredPath = command.split(" ")[1];
+  try {
+    let filePath = "";
+    if (enteredPath.split(path.sep).length === 1) {
+      await fs.access(path.join(currentDir, enteredPath));
+      filePath = path.join(currentDir, enteredPath);
+    } else {
+      await fs.access(enteredPath);
+      filePath = path.join(enteredPath);
+    }
+    const stat = await fs.lstat(filePath);
+    if (stat.isDirectory()) {
+      throw new Error();
+    }
+    const readStream = createReadStream(filePath, "utf-8");
+    readStream.on("data", function (chunk) {
+      console.log(chunk);
+    });
+  } catch (error) {
+    console.log(errorMessage);
+  }
+};
